9d4dfa4f01ba3405c70c185e5a772f02979871b0
Matthew Miller <mattmill1217@outlook.com>
2022-12-29 15:56:24 -0600

    made changes to init.py and main.py for simple formatting changes and then changed the main.py to look at the templates subfolder for the create_app function - site is up and running on local webserver

--- website/templates/__init__.py
+++ website/templates/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 #creates app for flask
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'uoihweiuhbewfuewhfwefhewfuih'
-    return app 
+    app.config['SECRET_KEY'] = 'uoihweiuhb ewfuewhfwefhewfuih'
+    return app
